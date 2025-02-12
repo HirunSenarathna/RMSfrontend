9c4d8428584422325b086a742f6cae0f2ba3f335
Hirun Senarathna <hirunsenarathna@users.noreply.github.com>
2025-02-12 10:39:36 +0530

    add menu items component

--- src/app/components/menu-items/menu-items.component.ts
+++ src/app/components/menu-items/menu-items.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export interface Product {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  salePrice?: number;
+}
+
 
 @Component({
   selector: 'app-menu-items',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu-items.component.html',
   styleUrl: './menu-items.component.css'
 })
 export class MenuItemsComponent {
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'Salad',
+      image: 'assets\salads1.jpg',
+      price: 699,
+      salePrice: 599
+    },
+    {
+      id: 2,
+      name: 'Orange juice',
+      image: 'assets/drinks1.jpg',
+      price: 1200
+    },
+    {
+      id: 3,
+      name: 'Rice & Curry',
+      image: 'assets/rnc2.jpg',
+      price: 199,
+      salePrice: 149
+    }
+  ];
 
+  addToCart(product: Product) {
+    console.log(`Added to cart: ${product.name}`);
+  }
 }
---
+++ src/app/services/product-service.service.ts
@@ -0,0 +1,21 @@
+// import { Injectable } from '@angular/core';
+// import { Observable, of } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ProductServiceService {
+
+//   constructor() { }
+
+//   private products = [
+//     { name: 'Spiced Chai', price: 3.99, image: 'assets/desserts1.jpg' },
+//     { name: 'Coconut Water', price: 2.99, image: 'assets/drinks1.jpg' },
+//     { name: 'Iced Lemon Tea', price: 3.49, salePrice: 2.62, image: 'assets/rnc2.jpg' },
+//     { name: 'Matcha Latte', price: 4.50, image: 'assets/matcha-latte.jpg' },
+//     { name: 'Mango Smoothie', price: 5.25, image: 'assets/mango-smoothie.jpg' }
+//   ];
+//   getProducts(): Observable<any[]> {
+//     return of(this.products);
+//   }
+// }
